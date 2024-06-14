826aaa7289334a85eb16b5e5ff85a382c1dc91ac
vladimir <vladimiryx2@gmail.com>
2024-06-13 19:11:24 -0500

    se realiza validacion al controlador coupons

--- Controllers/Coupons/CouponDeleteController.cs
+++ Controllers/Coupons/CouponDeleteController.cs
@@ -27,35 +27,43 @@ namespace Backend.Controllers.Coupons
         [Route("/delete/{id}")]
         public async Task<IActionResult> DeleteCoupon(int id)
         {
-            var userIdClaim = User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var userId = int.Parse(userIdClaim);
+            try
+             {
+                var userIdClaim = User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var userId = int.Parse(userIdClaim);
 
-            var coupon = _couponRepository.GetById(id);
+                var coupon = _couponRepository.GetById(id);
 
-            if (coupon == null)
-            {
-                return NotFound("Cupón no encontrado.");
+                 if (coupon == null)
+                {
+                    return NotFound("Cupón no encontrado.");
+                }
+
+                 if (coupon.EmployeeMarketingId != userId)
+                {
+                    return Forbid("No tienes permiso para eliminar este cupón.");
+                }
+
+                if (coupon.Status == "redimido")
+                {
+                    return BadRequest("El cupón no se puede eliminar porque ya ha sido utilizado.");
+                }
+
+                coupon.Status = "eliminado";
+
+                var result = await _couponRepository.UpdateCouponAsync(id, coupon, userId);
+                if (!result.IsSuccess)
+                 {
+                     return StatusCode(result.StatusCode, result.Message);
+                }
+
+                return Ok("Cupón eliminado correctamente.");
             }
-
-            if (coupon.EmployeeMarketingId != userId)
+            catch (Exception ex)
             {
-                return Forbid("No tienes permiso para eliminar este cupón.");
+                //the maneja la excepción según el status
+                return StatusCode(500, $"Ocurrió un error al eliminar el cupón.{ex.Message}");
             }
-
-            if (coupon.Status == "redimido")
-            {
-                return BadRequest("El cupón no se puede eliminar porque ya ha sido utilizado.");
-            }
-
-            coupon.Status = "eliminado";
-
-            var result = await _couponRepository.UpdateCouponAsync(id, coupon, userId);
-            if (!result.IsSuccess)
-            {
-                return StatusCode(result.StatusCode, result.Message);
-            }
-
-            return Ok("Cupón eliminado correctamente.");
         }
     }
 } 
